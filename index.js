bea7bd34e97d53ecf9c6c2e006e6666b23b450f6
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-10-22 01:17:56 +0100

    Initial checkin

---
+++ index.js
@@ -0,0 +1,76 @@
+var Plugin = require('broccoli-plugin');
+var path = require('path');
+var fs = require('fs');
+var RSVP = require('rsvp');
+var required = require('required');
+var symlinkOrCopySync = require('symlink-or-copy').sync;
+
+// Create a subclass NodeImporter derived from Plugin
+NodeImporter.prototype = Object.create(Plugin.prototype);
+NodeImporter.prototype.constructor = NodeImporter;
+
+function NodeImporter(inputNodes, options) {
+  options = options || {};
+  Plugin.call(this, inputNodes, {
+    annotation: options.annotation
+  });
+  this.options = options;
+  this.options.ignore = this.options.ignore || [];
+  this.options.mainFile = this.options.mainFile || 'index.js';
+}
+
+NodeImporter.prototype.build = function() {
+	this.npmPaths = [];
+	var indexPath = path.join(this.inputPaths[0], this.options.mainFile);
+
+	var output = "";
+	var nodeModulesOutPath = path.join(this.outputPath, 'node_modules');
+	fs.mkdirSync(nodeModulesOutPath);
+
+	var self = this;
+
+	return new RSVP.Promise(function(resolve, reject) {
+		required(indexPath, function(err, deps) {
+			if (err) {
+				reject(err);
+			}
+			else {
+		    self.linkDeps(deps, nodeModulesOutPath);
+		    resolve(null);
+		  }
+		});
+  });
+};
+
+NodeImporter.prototype.linkDeps = function(deps, nodeModulesOutPath) {
+	const moduleFolderRegex = /(.*\\node_modules\\)([^\\]*)\\.*/;
+	if (deps) {
+		var len = deps.length;
+		for (var i = 0; i < len; i++) {
+			var dependency = deps[i];
+			if (!dependency.core && 
+				  !this.options.ignore.contains(dependency.id))
+			{
+				var parts = moduleFolderRegex.exec(dependency.filename);
+				var folder = parts[2];
+		    if (this.npmPaths.indexOf(folder) === -1)
+		    {
+		    	// haven't seen this folder before
+		    	this.npmPaths.push(folder);
+		    	var modulePath = path.join(parts[1], folder);
+		    	this.linkNpmModule(modulePath, nodeModulesOutPath);
+				  // recurse
+			    this.linkDeps(dependency.deps);
+		    }
+			}
+		}
+	}
+}
+
+NodeImporter.prototype.linkNpmModule = function(folder, nodeModulesOutPath) {
+	var module = path.basename(folder);
+	moduleOutPath = path.join(nodeModulesOutPath, module);
+	symlinkOrCopySync(folder, moduleOutPath);
+}
+
+module.exports = NodeImporter;
