e9b1121cc620a26715c8ef7e677eb4b7ba9755e6
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2016-09-25 17:47:41 +0100

    Use require-traverser rather than required to correctly walk the require tree

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Plugin = require('broccoli-plugin');
 var path = require('path');
 var fs = require('fs');
 var RSVP = require('rsvp');
-var required = require('required');
+var requireTraverser = require('require-traverser');
 var Module = require('module').Module;
 var symlinkOrCopySync = require('symlink-or-copy').sync;
 var detective = require('detective');
@@ -26,127 +26,67 @@ function NodeImporter (inputNodes, options) {
 }
 
 NodeImporter.prototype.build = function () {
-	this.npmPaths = [];
-	this.linkedModules = [];
+	this.linkedModules = {};
 	var indexPath = path.join(this.inputPaths[0], this.options.mainFile);
 
+	var modulePath = this.inputPaths[0];
+	var moduleFile = indexPath;
 	fs.mkdirSync(path.join(this.outputPath, 'node_modules'));
 
 	var self = this;
 
 	return new RSVP.Promise(function(resolve, reject) {
-		required(indexPath, {
-				resolve: function (id, parent, cb) {
-					// Override required's default resolver to ensure we check nested modules.
-					var resolvedModule = Module._resolveLookupPaths(id, parent);
-					var paths = resolvedModule[1];
-
-					// Strip out folders up to the module path we're looking at.
-					var folder = getFolderFromPath(parent.filename);
-
-					if (this.filename == "C:\\Projects\\sc-pp\\sc-pp-backend\\node_modules\\request\\request.js") {
-						console.log("found paths: ");
-						console.log({paths});
-						console.log("looking for folder: " + folder);
-					}
-
-					while (paths.length)
-					{
-						if (paths[0].indexOf(folder) === 0)
-						{
-							break;
-						}
-
-						paths.shift();
-					}
-
-					var path = Module._findPath(id, paths);
-
-					if (this.filename == "C:\\Projects\\sc-pp\\sc-pp-backend\\node_modules\\request\\request.js") {
-						console.log("shifted paths: ");
-						console.log({shiftedPaths: paths});
-						console.log("found path: " + path);
-					}
-
-					cb(null, path);				
-				},
-				detective: detective
-			},
-			function (err, deps) {
-				if (err) {
-					reject(err);
-				}
-				else {
-					self.linkDeps(deps);
-					resolve(null);
-				}
+		var npmPaths = {};
+		requireTraverser(modulePath, moduleFile, function(err, files) {
+			if (err) {
+				reject(err);
 			}
-		);
-	});
-};
-
-NodeImporter.prototype.linkDeps = function (deps) {
-	if (deps)
-	{
-		const len = deps.length;
-		for (var i = 0; i < len; i++)
-		{
-			var dependency = deps[i];
-			if (!dependency.core && !this.options.ignore.contains(dependency.id))
+			for (file in files)
 			{
-				// if dependency.filename is a folder, use that else, use the folder it is in
+				var filename = path.basename(file);
+				// if file is a folder, use that else, use the folder it is in
 
-				var folder = getFolderFromPath(dependency.filename);
-				if (this.npmPaths.indexOf(folder) === -1)
+				var folder = getFolderFromPath(file);
+				if (!npmPaths[folder])
 				{
 					// haven't seen this folder before
-					this.npmPaths.push(folder);
-
-					// Link the module if it's at the root of the node_modules directory.
-					if (this.isRootModule(dependency.filename))
-					{
-						this.linkNpmModule(folder);
-					}
+					npmPaths[folder] = true;
 				}
-
-				// recurse
-				this.linkDeps(dependency.deps);
 			}
-		}
-	}
-}
 
-NodeImporter.prototype.isRootModule = function (modulePath) {
-	// Root if only one occurrence of node_modules.
-	return (modulePath.match(/[\\\/]node_modules[\\\/]/g) || []).length === 1;
+			resolve(Object.keys(npmPaths));
+		})
+	})
+	.then(npmPaths => {
+		for (var npmPath of npmPaths) {
+			this.linkNpmModule(npmPath);
+		}
+	});
 };
 
 NodeImporter.prototype.linkNpmModule = function (folder) {
 	// Strip the leading path off up to the first occurrence of node_modules,
 	// and remove everything after the root module name.
 	var nodeModulesIndex = folder.indexOf("node_modules");
+	if (nodeModulesIndex === -1)
+	{
+		// this is not a node module
+		return;
+	}
 	var basePath = folder.substr(0, nodeModulesIndex);
 	var folderRelativePath = folder.substr(nodeModulesIndex);
 	var modulePathParts = folderRelativePath.split(path.sep);
 	var moduleRelativePath = path.join("node_modules", modulePathParts[1]);
 	var moduleOutPath = path.join(this.outputPath, moduleRelativePath);
 
-	// Don't bother if we've already linked this module.
-	if (this.linkedModules.indexOf(moduleOutPath) > -1)
+	// Don't bother if we've already linked this module or we're ignoring it.
+	if (this.linkedModules[moduleOutPath] || this.options.ignore.contains(modulePathParts[1]))
 	{
 		return;
 	}
 
-	try
-	{
-		fs.statSync(moduleOutPath); // throws if path doesn't exist
-	}
-	catch (e)
-	{
-		// fill the hole
-		symlinkOrCopySync(path.join(basePath, moduleRelativePath), moduleOutPath);
-		this.linkedModules.push(moduleOutPath);
-	}
+	symlinkOrCopySync(path.join(basePath, moduleRelativePath), moduleOutPath);
+	this.linkedModules[moduleOutPath] = true;
 };
 
 module.exports = NodeImporter;
